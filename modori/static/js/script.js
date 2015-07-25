dfddff24d188583a4591bf4cfea6c9d9c0d996ce
Keon Woo Kim <kwk236@gmail.com>
2015-07-25 03:19:18 -0400

    frontend fix

--- modori/static/js/script.js
+++ modori/static/js/script.js
@@ -1,3 +1,35 @@
-  $('.carousel').carousel({
-    interval: 3500
-  })
+  // $('.carousel').carousel({
+  //   interval: 3500
+  // })
+
+  // 
+
+
+$(function(){
+	$('button#send').bind('click', function(){
+
+
+		var data = {
+			"text":$('textarea[name="text"]').val()
+		}
+		console.log(data);
+
+		$.ajax({
+			type:"POST",
+			url:"/api/pos",
+			dataType:'json',
+			encode:true,
+			data:data,
+
+		})
+		.done(function(data){
+			$('#result').text(data);
+			console.log(data);
+		})
+		.fail(function(data){
+			$('#result').html('<p>An error has occured</p>');
+			console.log(data);
+		});
+
+	});
+})
